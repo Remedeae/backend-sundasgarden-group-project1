3d572f7148e67f7bfcbbe3adb11d05308cb46482
Remedeae <sanna.m.blomdahl@gmail.com>
2025-09-11 00:39:06 +0200

    finished the preliminary html structure, removed the pages map and moved out to src

--- src/components/taskItem.tsx
+++ src/components/taskItem.tsx
@@ -3,15 +3,20 @@ import TaskTag from "./taskTag";
 function TaskItem() {
     <div className="task">
         <div className="task__info">
-        <h3>{props.task}</h3>
-        <p>{props.taskDescription}</p>
-        <div className="task__tags">
-            
-        </div>
+            <h3>{props.task}</h3>
+            <p>{props.taskDescription}</p>
+            <div className="task__tags">
+
+            </div>
 
         </div>
-        <div className="task__check-done">
-            <i></i>
+        <div className="task__action">
+            <div className="task__action-done">
+                <i></i>
+            </div>
+            <div className="task__action-trash">
+                <i></i>
+            </div>
         </div>
     </div>
 }
