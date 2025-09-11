1cceb86b7212d8ffa6bc0723b60da14c3e65c4d9
Remedeae <sanna.m.blomdahl@gmail.com>
2025-09-11 15:45:17 +0200

    added functions for adding items to the to-do list, added placeholder cat picture, added folder and file to import types from

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,37 @@
-import taskItem from './components/taskItem.tsx'
+import { useState } from 'react'
+import TaskItem from './components/taskItem.tsx'
 import TaskTag from './components/taskTag.tsx'
 import WeatherItem from './components/weatherItem.tsx'
 
+import catPlaceholder from './assets/cat-placeholder.jfif'
+
+import type { tasksType } from './interfaces/taskType.ts'
 
 function App() { //Note that this code doesn't mount right now due to undefined variables
+
+
+
+  const initialTasks: tasksType[] = [];
+  const emptyTask: tasksType = {
+    titel: "",
+    description: "",
+    tags: "",
+  }
+  const [tasks, setTasks] = useState<tasksType[]>(initialTasks);
+  const [newTask, setNewTask] = useState<tasksType>(emptyTask);
+
+  const handleTaskChange = (e: React.ChangeEvent<HTMLInputElement>, field : keyof tasksType) => {
+    setNewTask({
+      ...newTask,
+      [field]: e.target.value
+    });
+  }
+
+  const handleAddTask = () => {
+    setTasks((t) => [...t, newTask]);
+    setNewTask(emptyTask);
+  }
+
   return (
     <div className='app'>
       <div className="apiContent">
@@ -15,32 +43,36 @@ function App() { //Note that this code doesn't mount right now due to undefined 
         </div>
         <div className="catPic">
           <h2> <i></i>Daily Cat</h2>
-          <img src={catPic} alt="Cat of the Day" />
+          <img src={catPlaceholder} alt="Cat of the Day" />
           <p><i></i>Photo of teh Day</p>
 
         </div>
       </div>
       <div className="toDo">
-        <h2> <i></i>Active Tasks {activeTaskCount}</h2>
-        <div className='toDo__list'>
-
-        </div>
+        <h2> <i></i>Active Tasks {/* activeTaskCount */}</h2>
+        <ul className='toDo__list'>
+          {tasks.map((task, index) => (
+            <li key={index}>
+              <TaskItem titel={task} description='' tags='' />
+            </li>
+          ))}
+        </ul>
       </div>
       <div className="taskManagment">
         <div className="addTask">
           <h2><i></i>Add New Task</h2>
           <div className='addTask__input'>
             <label htmlFor="newTask">Task</label>
-            <input type="text" id="newTask" placeholder='Enter task...' value={newTask} onChange={(e) => setNewTask(e.target.value)} />
+            <input type="text" id="newTask" placeholder='Enter task...' value={newTask.titel} onChange={(e=>handleTaskChange(e, "titel"))} />
             <label htmlFor="taskDescription">Description</label>
-            <input type="text" id="taskDescription" placeholder='Enter task description...' value={taskDescription} onChange={(e) => setTaskDescription(e.target.value)} />
+            <input type="text" id="taskDescription" placeholder='Enter task description...' value={newTask.description} onChange={(e)=>handleTaskChange(e, "description")} />
             <label htmlFor="taskTag">Tags</label>
             <input type="checkbox" id='taskTag' />
             <button onClick={handleAddTask}>Add task</button>
           </div>
         </div>
         <div className="completedTasks">
-          <h2><i></i>Completed Tasks {completedTaskCount}</h2>
+          <h2><i></i>Completed Tasks {/* completedTaskCount */}</h2>
           <div className='completedTasks__list'>
 
           </div>
